573874f067d21c81a2a6f167eca62509bb5afab9
tylim <limteikyee@gmail.com>
2025-04-13 15:39:09 +0800

    merge mantine and tailwind

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,39 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-import unusedImports from 'eslint-plugin-unused-imports';
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import unusedImports from 'eslint-plugin-unused-imports'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),{
-    extends: [
-      eslintPluginPrettier,
-    ],
-    plugins: {
-      'unused-imports': unusedImports,
-    },
-    rules: {
-      '@typescript-eslint/no-unused-vars': 'off', // let unused-imports/no-unused-imports take over instead
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-        },
-      ],
-      'unused-imports/no-unused-imports': 'warn',
-    },
-  }
-];
+	...compat.extends(
+		'next/core-web-vitals',
+		'next/typescript',
+		'plugin:eslint-plugin-prettier/recommended',
+	),
+	{
+		plugins: {
+			'unused-imports': unusedImports,
+		},
+		rules: {
+			'@typescript-eslint/no-unused-vars': 'off', // let unused-imports/no-unused-imports take over instead
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+				},
+			],
+			'unused-imports/no-unused-imports': 'warn',
+		},
+	},
+]
 
-export default eslintConfig;
+export default eslintConfig
